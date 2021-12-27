127f049100b99cd6e5b7730ebd2803cf86a38fdb
Ömer Faruk Özdemir <farukozderim@gmail.com>
2021-12-27 13:13:47 +0300

    - Support feature is added.

--- src/listener.py
+++ src/listener.py
@@ -175,6 +175,12 @@ async def listen_telegram_messages(message: MessageBodyModel):
             else:
                 response_message = f"I could not forward your feedback to the admin, an error occurred."
 
+    elif first_word == "support" or first_word == "/support":
+        response_message = (f"Thank you, to support me you can"
+                            f"\n- Share me with your friends"
+                            f"\n- Give feedback using the command, *feedback sentence*."
+                            f"\n- Star the github repository at https://github.com/FarukOzderim/Memory-Vault/"
+                            )
     else:
         response_message = (
             f"{name}, I do not know that command.{Constants.HELP_MESSAGE}"
