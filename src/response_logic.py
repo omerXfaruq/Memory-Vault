8a46ee8a67a3ba079b356575f0711bd926fac7aa
Ömer Faruk Özdemir <farukozderim@gmail.com>
2022-01-08 18:50:48 +0300

    edit the list response message

--- src/response_logic.py
+++ src/response_logic.py
@@ -78,7 +78,8 @@ class ResponseLogic:
                 return "No memory found in the system. Please add memory, for further information you can type help or */help*."
             else:
                 response_message = (
-                    f"Open the gates of the memory vault! You will receive a message every second"
+                    f"Brace yourself, you will receive a message every second."
+                    f"\nOpen the gates of the memory vault!"
                     f"\n*id | memory*"
                 )
 
