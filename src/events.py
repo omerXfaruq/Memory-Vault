00cd5c723f9e7c5c837fcd36b817cdd1c147e8d2
Ömer Faruk Özdemir <farukozderim@gmail.com>
2022-01-08 20:43:09 +0300

    do not sleep if 0

--- src/events.py
+++ src/events.py
@@ -75,8 +75,8 @@ class Events:
 
     @classmethod
     async def send_a_message_to_user(cls, telegram_id: int, message: str, retry_count: int = 5, sleep_time: float = 0) -> bool:
-        await asyncio.sleep(sleep_time)
-        print(sleep_time)
+        if sleep_time != 0:
+            await asyncio.sleep(sleep_time)
         message = ResponseToMessage(
             **{
                 "text": message,
